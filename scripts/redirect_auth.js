45687e0b25111441ddbacec147119d2876b3ac4e
Joao Vieira <joaoantoniopintovieira13@gmail.com>
2021-09-10 10:46:41 +0100

    first commit

---
+++ scripts/redirect_auth.js
@@ -0,0 +1,7 @@
+document.getElementById("auth").onclick = function () {
+    location.href = "/article-auth.html";
+};
+
+document.getElementById("auth-legionela").onclick = function () {
+    location.href = "/article-auth-legionela.html";
+};
